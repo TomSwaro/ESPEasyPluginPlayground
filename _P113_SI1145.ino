36d7f710d24d7078503f5f03fff410c20073d335
TomSwaro <37219072+TomSwaro@users.noreply.github.com>
2018-03-24 16:58:54 +0100

    Update _P113_SI1145.ino
    changed sensor type because MQTT controllers reported only one Value : SENSOR_TYPE_TRIPLE

--- _P113_SI1145.ino
+++ _P113_SI1145.ino
@@ -283,7 +283,7 @@ boolean Plugin_113(byte function, struct EventStruct *event, String& string)
       {
         Device[++deviceCount].Number = PLUGIN_ID_113;
         Device[deviceCount].Type = DEVICE_TYPE_I2C;
-        Device[deviceCount].VType = SENSOR_TYPE_SINGLE;
+        Device[deviceCount].VType = SENSOR_TYPE_TRIPLE;
         Device[deviceCount].Ports = 0;
         Device[deviceCount].PullUpOption = false;
         Device[deviceCount].InverseLogicOption = false;
